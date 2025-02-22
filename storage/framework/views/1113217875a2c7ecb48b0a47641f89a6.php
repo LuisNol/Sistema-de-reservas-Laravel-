2e095ac9f2b24ecd0ce64afbbcb52231ffe26597
Nolberto Luis Sumaran Pimentel <nolberto.sumaran@gmail.com>
2025-02-22 14:02:27 -0500

    first commit

---
+++ storage/framework/views/1113217875a2c7ecb48b0a47641f89a6.php
@@ -0,0 +1,9 @@
+<div class="sidebar-widget">
+    <div class="sidebar-title">
+        <h2><?php echo e($item->title); ?></h2>
+    </div>
+    <div class="textwidget">
+        <?php echo e($item->content); ?>
+
+    </div>
+</div><?php /**PATH C:\laragon\www\Gotrip.1.6.1\themes/GoTrip/News/Views/frontend/layouts/sidebars/content_text.blade.php ENDPATH**/ ?>
